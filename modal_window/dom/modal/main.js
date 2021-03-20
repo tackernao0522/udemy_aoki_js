8ac83da03981ca19b98c192956c81cc5807144cf
takaki <cheap_trick_magic@yahoo.co.jp>
2021-03-20 14:45:16 +0900

    モーダルウインドウ

--- modal_window/dom/modal/main.js
+++ modal_window/dom/modal/main.js
@@ -5,16 +5,16 @@ const show = document.getElementById('modal__show');
 const close = document.getElementById('modal__close');
 const backGround = document.getElementById('modal__background');
 
-show.addEventListener('click', () => {
-  modal.classList.add('is-show');
-  backGround.classList.add('is-show');
+show.addEventListener('click', () => { /* #modla__showをクリックすると */
+  modal.classList.add('is-show'); /* .modalに .is-showが追加される */
+  backGround.classList.add('is-show'); /* .modal__backgroundに .is-showが追加される */
 })
 
-close.addEventListener('click', () => {
-  modal.classList.remove('is-show');
-  backGround.classList.remove('is-show');
+close.addEventListener('click', () => { /* #modal__closeをクリックすると */
+  modal.classList.remove('is-show'); /* .is-showが消される */
+  backGround.classList.remove('is-show'); /* .is-showも消される */
 })
 
-backGround.addEventListener('click', () => {
-  close.click();
+backGround.addEventListener('click', () => { /* #modal__showがクリックされると */
+  close.click(); /* is-showが消される */
 })
