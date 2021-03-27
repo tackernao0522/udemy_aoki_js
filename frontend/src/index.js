aaf9141996167cdf41cfadc9b83d869b5e094dd3
takaki <cheap_trick_magic@yahoo.co.jp>
2021-03-27 14:42:44 +0900

    Polyfill fetch

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import './style.scss';
 require('intersection-observer');
+import 'whatwg-fetch';
 
 console.log('webpackテストです');
 
@@ -7,24 +8,52 @@ console.log('webpackテストです');
 //   alert('babelテストです');
 // });
 
-//window.addEventListener('scroll')
+const url = 'http://coinbaby8.com/udemy_js_api/api/udemy_js';
 
-const options = {
-  threshold: 0.5,
-  rootMargin: '-200px'
+const get_options = {
+  method: 'get'
 };
 
-const observer = new IntersectionObserver(callback, options); // ブラウザの画面に交わったらcallbackが実行される
+const getFormData = fetch(url, get_options)
+  .then(response => response.json());
+console.log(getFormData);
 
-const targets = document.querySelectorAll('.message-box');
+const postForm = document.forms.post_form; // フォームの情報を取得している
+const formData = new FormData(postForm); // フォームデータオブジェクトを作って取得しているフォーム情報を引数にしている
+// console.log(formData);
 
-targets.forEach(target => observer.observe(target));
+// URLを生成するサンプル
+// 日本語はURLエンコードが必要
+const your_name = encodeURI(formData.get('your_name'));
 
-function callback(entries) {
-  entries.forEach(entry => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-    }
+const stringJoin = '?' + 'your_name' + '=' + your_name + '&';
+console.log(stringJoin);
+
+// entriesメソッドを使うことによって中身が見えるようになる
+// for (let fd of formData.entries()) {
+// console.log(`${fd[0]}: ${fd[1]}`); // inputの初期値(value)が表示される
+// }
+
+document.getElementById('button')
+  .addEventListener('click', () => {
+    const postForm = document.forms.post_form;
+    const formData = new FormData(postForm);
+
+    const url = 'http://coinbaby8.com/udemy_js_api/api/udemy_js';
+
+    const post_options = {
+      method: 'post',
+      body: formData
+    };
+
+    fetch(url, post_options)
+      .then(response => {
+        if (response.ok) {
+          return response.text();
+        } else {
+          return new Error();
+        }
+      })
+      .then(text => console.log(text))
+      .catch(error => console.log(error));
   });
-}
