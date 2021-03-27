c14b50b633f0d1d70321add4bc950c3db9830820
takaki <cheap_trick_magic@yahoo.co.jp>
2021-03-27 13:55:04 +0900

    Polyfill Intersection Observer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,30 @@
 import './style.scss';
+require('intersection-observer');
 
 console.log('webpackテストです');
 
-window.addEventListener('load', () => {
-  alert('babelテストです');
-});
+// window.addEventListener('load', () => {
+//   alert('babelテストです');
+// });
+
+//window.addEventListener('scroll')
+
+const options = {
+  threshold: 0.5,
+  rootMargin: '-200px'
+};
+
+const observer = new IntersectionObserver(callback, options); // ブラウザの画面に交わったらcallbackが実行される
+
+const targets = document.querySelectorAll('.message-box');
+
+targets.forEach(target => observer.observe(target));
+
+function callback(entries) {
+  entries.forEach(entry => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}
